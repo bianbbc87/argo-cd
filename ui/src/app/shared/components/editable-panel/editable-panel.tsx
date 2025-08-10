8e7d0c9a268d92cf2d248352f372b91ab7d69c0d
EunJiJung <bianbbc87@gmail.com>
2025-08-10 20:15:38 +0900

    fix: gate Edit/Save/Cancel with registered !== false in existing && chain
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- ui/src/app/shared/components/editable-panel/editable-panel.tsx
+++ ui/src/app/shared/components/editable-panel/editable-panel.tsx
@@ -32,7 +32,7 @@ export interface EditablePanelProps<T> {
     collapsible?: boolean;
     collapsed?: boolean;
     collapsedDescription?: string;
-    registered: boolean;
+    registered?: boolean;
 }
 
 interface EditablePanelState {
@@ -75,11 +75,9 @@ export class EditablePanel<T = {}> extends React.Component<EditablePanelProps<T>
                         </div>
                     ) : (
                         <div className={classNames('white-box editable-panel', {'editable-panel--disabled': this.state.saving})}>
-                            {this.props.registered && this.props.floatingTitle && (
-                                <div className='white-box--additional-top-space editable-panel__sticky-title'>{this.props.floatingTitle}</div>
-                            )}
+                            {this.props.floatingTitle && <div className='white-box--additional-top-space editable-panel__sticky-title'>{this.props.floatingTitle}</div>}
                             <div className='white-box__details'>
-                                {!this.props.noReadonlyMode && this.props.save && (
+                                {this.props.registered !== false && !this.props.noReadonlyMode && this.props.save && (
                                     <div className='editable-panel__buttons' style={{right: this.props.collapsible ? '5em' : '3em'}}>
                                         {!this.state.edit && (
                                             <button
